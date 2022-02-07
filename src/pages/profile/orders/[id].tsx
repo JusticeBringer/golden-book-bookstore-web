159350194fa3dd1267d3dbf549ec3866059fa42b
justicebringer <gabithebigg@gmail.com>
2022-02-07 18:43:28 +0200

    Change rendering of order id page

--- src/pages/profile/orders/[id].tsx
+++ src/pages/profile/orders/[id].tsx
@@ -1,50 +1,51 @@
-import { GetStaticProps, GetStaticPaths } from 'next';
+import { GetServerSideProps, GetStaticPaths } from 'next';
 import { Box, Flex } from '@chakra-ui/react';
 import { OrderDocument } from '../../../database/models/order/order.interface';
 import axios from 'axios';
 import OrderDetails from '../../../components/SubComponents/OrderDetails';
 import BookDocument from '../../../database/models/book/book.interface';
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const ordersApiUrl = process.env.DOMAIN_URL_API_ORDERS;
-  const headers = {
-    authorization: process.env.SECRET_JWT_TOKEN
-  };
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const ordersApiUrl = process.env.DOMAIN_URL_API_ORDERS;
+//   const headers = {
+//     authorization: process.env.SECRET_JWT_TOKEN
+//   };
 
-  let orders: OrderDocument[] = [];
-  await axios
-    .get<OrderDocument[]>(ordersApiUrl, { headers })
-    .then(response => (orders = response.data))
-    .catch(error => {
-      console.log(error);
-      throw new Error(error);
-    });
+//   let orders: OrderDocument[] = [];
+//   await axios
+//     .get<OrderDocument[]>(ordersApiUrl, { headers })
+//     .then(response => (orders = response.data))
+//     .catch(error => {
+//       console.log(error);
+//       throw new Error(error);
+//     });
 
-  const paths = orders.map(order => {
-    return {
-      params: { id: order._id.toString() }
-    };
-  });
+//   const paths = orders.map(order => {
+//     return {
+//       params: { id: order._id.toString() }
+//     };
+//   });
 
-  if (process.env.NODE_ENV !== 'production') {
-    return {
-      paths,
-      fallback: 'blocking'
-    };
-  } else {
-    return {
-      paths,
-      fallback: 'blocking'
-    };
-  }
-};
+//   if (process.env.NODE_ENV !== 'production') {
+//     return {
+//       paths,
+//       fallback: 'blocking'
+//     };
+//   } else {
+//     return {
+//       paths,
+//       fallback: 'blocking'
+//     };
+//   }
+// };
 
 type DetailsProps = {
   order: OrderDocument;
   books: BookDocument[];
 };
 
-export const getStaticProps: GetStaticProps = async context => {
+// Change to getStaticProps on hosting providers that support it
+export const getServerSideProps: GetServerSideProps = async context => {
   const id = context.params.id;
 
   const orderDetailsApiUrl = process.env.DOMAIN_URL_API_ORDERS + '/' + id.toString();
@@ -75,8 +76,7 @@ export const getStaticProps: GetStaticProps = async context => {
     props: {
       order: res,
       books: books
-    },
-    revalidate: 1
+    }
   };
 };
 
